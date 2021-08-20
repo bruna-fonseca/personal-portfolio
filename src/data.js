1d54accb7ea8f35ae3ad1f1ba5c07517126c2d87
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-08-20 16:18:32 -0300

    deploy

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Rick and Morty",
+    subtitle: "Buscador de personagens",
+    description:
+      "Projeto para praticar React Hooks, consumindo uma API sobre a série de desenho Rick and Morty",
+    image: "./rickandmorty.png",
+    link: "https://rickandmortybruna-project.netlify.app/",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "O Letmeask é uma aplicação para criação de salas de Q&A, a fim de utilizar em ocasiões onde é preciso uma maior organização para perguntar e responder questões.",
+    image: "./nlw-letmeask.png",
+    link: "https://letmeask---nlw---together.web.app/",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Mysql",
+  "Node.js",
+  "MongoDB",
+  "Python",
+];
